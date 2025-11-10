24dee657720d8e8efcf7e1dfea35376e6fc5e518
sk0luu <cxasx2006@alumnes.ilerna.com>
2025-11-10 20:38:20 +0100

    Primer commit - archivo HolaGit.java

--- HolaGit.java
+++ HolaGit.java
@@ -1,27 +1,5 @@
-hola
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-ivan
-
+public class HolaGit {
+    public static void main(String[] args) {
+        System.out.println("Hola Git!");
+    }
+}
